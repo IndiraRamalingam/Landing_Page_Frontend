86a96a04fec3acca4a04d8b0c24a5614c1a7e5d5
IndiraRamalingam <indiraramalingam93@gmail.com>
2024-01-07 04:41:46 +0530

    Completed till CallToAction

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function CallToAction() {
   return (
-    <div>CallToAction</div>
+    <>
+    <div className='container' style={{marginTop:'80px'}}>
+        <div className="featuretxt">
+                <div className='overlay-feature'>
+                <p className='fPara1'>Unlock Limitless Possibilities with Our New Card Solutions</p>
+                <div className='m-3' style={{textAlign:'center'}}>
+                  <button className='btn btn-purple m-1'>Unlock your card <span><i class="fa fa-credit-card" aria-hidden="true"></i></span></button>
+                  <button className='btn btn-light m-1'>Customer Support <span><i class="fa fa-headphones" aria-hidden="true"></i></span></button>
+                </div>
+                </div>
+        </div>
+    </div>
+  
+    </>
   )
 }
 
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Wise from '../assets/LogoCloud/Wise.jpg'
+import Testimonial1 from '../assets/Testimonial/Testimonial.jpg'
 
 function Testimonial() {
   return (
     <>
-    <div className='container'>
-      Testimonial
+    <div className='container' style={{marginTop:'80px'}}>
+    <div className='testimonial'>
+      <div  style={{textAlign:'center'}}>
+        <img src={Wise} alt="Wise"/>
+      </div>
+      <p className='tPara m-5'>I had the pleasure of experiencing the next generation of card solutions with this incredible product. It's refreshing to see such innovation in the financial industry.</p>
+      <div style={{textAlign:'center'}}>
+        <img src={Testimonial1} />
+        <h5 className='m-3'>Nick Babich</h5>
+        <p>⭐ ⭐ ⭐ ⭐</p>
+      </div>
+    </div>  
     </div>
     </>
   )
